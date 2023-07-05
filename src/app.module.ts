ed10468fa79ce654040d84fc4b9f93e3ae30436f
Tuan Pham <tuanpham@chidoanh.com>
2023-07-05 15:24:56 +0700

    Middleware logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOption } from 'db/data-source';
+import { logger } from './common/middleware/logger.middleware';
+import { TodoController } from './todo/todo.controller';
 
 @Module({
   imports: [
@@ -13,4 +15,10 @@ import { dataSourceOption } from 'db/data-source';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .forRoutes(TodoController);
+  }
+}
